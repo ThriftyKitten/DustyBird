2d190c65c1879cfdefdb113599d1b1cf814096b6
Michael Scofield <michaelscofswork@gmail.com>
2017-08-29 13:46:19 -0700

    switching to java.utils import for compatability with api 23

--- app/src/main/java/dustybirdapps/cat_a_day/DisplayCat.java
+++ app/src/main/java/dustybirdapps/cat_a_day/DisplayCat.java
@@ -3,6 +3,7 @@ package dustybirdapps.cat_a_day;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import java.util.Calendar;
 import android.widget.ImageView;
 
 
@@ -13,20 +14,22 @@ public class DisplayCat extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_cat);
 
-        //int day_of_year = calendar.get(Calendar.DAY_OF_YEAR);
+        // get the day of year using calendar
+        Calendar calendar = Calendar.getInstance();
+        int day_of_year = calendar.get(Calendar.DAY_OF_YEAR);
         // turn day of year into string
 
-        //String day_of_year_string = Integer.toString(day_of_year);
+        String day_of_year_string = Integer.toString(day_of_year);
         // append cat and day of year string to get cat#
-        //String cat_string = "cat";
-        //String cat_image_string = cat_string + day_of_year_string;
+        String cat_string = "cat";
+        String cat_image_string = cat_string + day_of_year_string;
 
         // need drawable resource number, finding the cat drawable resource id
-        //int ResId = R.getIdentifier(cat_image_string, "drawable", getPackageName());
+        int ResId = R.getIdentifier(cat_image_string, "drawable", getPackageName());
         //Drawable cat_image = R.getDrawable(ResId);
         // Capture the layout's "Picture frame" and put catimage in it
-        //ImageView imageView = (ImageView) findViewById(R.id.imageView);
-        //imageView.setImageResource(getImageId(this, cat_image_string));
+        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+        imageView.setImageResource(getImageId(this, cat_image_string));
 
     }
 
